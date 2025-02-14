1e57c4dbcd997277a7f99ba13db46c3e8d4c6b49
Tony Beeman <beeman@gmail.com>
2025-02-14 00:59:25 -0800

    Add logo

--- test/server.py
+++ test/server.py
@@ -18,6 +18,12 @@ class Handler(http.server.SimpleHTTPRequestHandler):
         print(f"Error {code}: {self.path}")
         super().send_error(code, message, explain)
 
+    def end_headers(self):
+        self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
+        self.send_header('Pragma', 'no-cache')
+        self.send_header('Expires', '0')
+        super().end_headers()
+
 if __name__ == "__main__":
     os.chdir(DIRECTORY)
     # Output the path of DIRECTORY
